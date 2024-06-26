e57de2f030b2e4fa5f28736fda7780a439dbb11e
vaeian <857669126@qq.com>
2024-06-27 01:14:44 +0800

    fix: deploy config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   nitro: {
-    preset: 'vercel'
+    // preset: 'vercel'
   },
   devtools: { enabled: true },
   css: ['~/assets/global.scss'],
