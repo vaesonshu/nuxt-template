3478b1929af9ce1022230aabac411b248813fab9
vaesonshu <g857669126@163.com>
2024-11-01 15:32:35 +0800

    fix: test nuxthub deploy

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="text-xl text-center">
-    <!-- <div>{{ message }}</div>
-    <div class="text-sm">一款开箱即用的 Nuxt 项目模板，您无需在项目初始化时进行各种配置，我们已经帮您进行必要的配置</div> -->
+    <div>{{ message }}</div>
+    <div class="text-sm">一款开箱即用的 Nuxt 项目模板，您无需在项目初始化时进行各种配置，我们已经帮您进行必要的配置</div>
   </div>
 </template>
 <script setup lang="ts">
-// const { message } = await $fetch('/api/hello')
+const { message } = await $fetch('/api/hello')
 </script>
 
 <style scoped lang="scss"></style>
